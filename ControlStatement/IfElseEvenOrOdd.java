d9a2cfe66b60fce2088bcf7dc3beaf15ecd1ebd6
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-10 14:41:18 +0530

    Add files via upload

---
+++ ControlStatement/IfElseEvenOrOdd.java
@@ -0,0 +1,11 @@
+public class IfElseEvenOrOdd {
+    public static void main(String[] args) {
+        int num=438;
+
+        if(num%2==0){
+            System.out.println("num is even");
+        }else{
+            System.out.println("num is odd");
+        }
+    }
+}
