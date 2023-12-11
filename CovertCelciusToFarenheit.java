a014d9a6f2b4235a003b30a77d9e7f37c0140f14
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-11 21:29:00 +0530

    Delete CovertCelciusToFarenheit.java

--- CovertCelciusToFarenheit.java
+++
@@ -1,8 +0,0 @@
-//program to convert celcius to farenheit 
-public class CovertCelciusToFarenheit {
-    public static void main(String[] args) {
-        int c=5;
-        int f=32+(9/5)*c;
-        System.out.println("conversion of "+c +" in farenheit="+f);
-    }
-}
