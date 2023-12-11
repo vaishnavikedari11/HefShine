1e13f8756a4bd096cb836aad629cae00df62f3c6
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-11 21:27:03 +0530

    Delete PrintWithoutUsingTwoSysout.java

--- PrintWithoutUsingTwoSysout.java
+++
@@ -1,8 +0,0 @@
-//print Hef
-//      Shine on two lines using only one sysout statement
-public class PrintWithoutUsingTwoSysout {
-    public static void main(String[] args) {
-        System.out.println("Hef \nShine");
-        
-    }
-}
