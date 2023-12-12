190313ec26abebbac83ded6b8579b941e91cd4eb
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-12 16:50:57 +0530

    Add files via upload

---
+++ ControlStatement/IfCheckNum.java
@@ -0,0 +1,10 @@
+public class IfCheckNum {
+    public static void main(String[] args) {
+        int num=7;
+        if(num>=1 && num<=10){
+            System.out.println("yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
