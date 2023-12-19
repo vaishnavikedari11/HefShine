ef3f065c155e3f3463bdcbc366ddfab4a1244af6
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-19 15:27:56 +0530

    Add files via upload

---
+++ ControlStatement/For_Loop/DigitRange.java
@@ -0,0 +1,9 @@
+public class DigitRange {
+    public static void main(String[] args) {
+        for(int i=100;i<=200;i++){
+            if(i%10==7){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ ControlStatement/For_Loop/NestedFor.java
@@ -0,0 +1,13 @@
+public class NestedFor {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++){
+            
+                System.out.println("Hi");
+
+            }
+        }
+
+        
+    }
+}
