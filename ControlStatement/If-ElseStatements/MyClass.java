8eb6463610b78b35ef9af4a439edd83719c4467e
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-22 10:06:06 +0530

    Delete ControlStatement/If-ElseStatements directory

--- ControlStatement/If-ElseStatements/MyClass.java
+++
@@ -1,9 +0,0 @@
-class MyClass{
-    public static void main(String[] args) {
-        int side=5;
-        int area=side*side;
-        int perimeter=4*side;
-        System.out.println("Area of square is="+area);
-        System.out.println("Perimeter of square is="+perimeter);
-    }
-}
--- ControlStatement/If-ElseStatements/TotalAverage.java
+++
@@ -1,7 +0,0 @@
-public class TotalAverage {
-    public static void main(String[] args) {
-        int s1=90, s2=80, s3=89, s4=90 ,s5=88;
-        int totalAverage=(s1+s2+s3+s4+s5)/5;
-        System.out.println(totalAverage);
-    }
-}
