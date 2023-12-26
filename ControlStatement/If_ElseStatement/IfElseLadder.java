3ad4cb4e4fd2e5d2a0a62c61fc6ec124ce433687
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-26 10:11:47 +0530

    Add files via upload

---
+++ ControlStatement/If_ElseStatement/IfElseLadder.java
@@ -0,0 +1,12 @@
+public class IfElseLadder {
+    public static void main(String[] args) {
+        int num=34;
+        if(num>0){
+            System.out.println("num is positive");
+        }else if(num<0){
+            System.out.println("num is negative");
+        }else{
+            System.out.println("num is zero");
+        }
+    }
+}
